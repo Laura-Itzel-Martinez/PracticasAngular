94102319041216a9346efc04d536dbb2ae147055
itzel <itzelmartinez455@gmail.com>
2021-11-19 15:56:47 -0600

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+
+import { Component,ViewChild } from '@angular/core';
+import { SelectornumericoComponent} from './selectornumerico/selectornumerico.component'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,488 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EjercicioU4';
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)    
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*@ViewChild('selector1')selector1!: SelectornumericoComponent;
+  @ViewChild('selector2') selector2!: SelectornumericoComponent;  
+
+  fijarSelector1(valor:number) {
+    this.selector1.fijar(valor);
+  }
+
+  fijarSelector2(valor:number) {
+    this.selector2.fijar(valor);
+  }*/
+  /*articulos = [{codigo:1, descripcion:'papas', precio:10.55},
+               {codigo:2, descripcion:'manzanas', precio:12.10},
+               {codigo:3, descripcion:'melon', precio:52.30},
+               {codigo:4, descripcion:'cebollas', precio:17},
+               {codigo:5, descripcion:'calabaza', precio:20},
+              ];*/
+  
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*@ViewChild('selector1')selector1!: SelectornumericoComponent;
+  @ViewChild('selector2') selector2!: SelectornumericoComponent;
+  
+  fijarSelector1(valor:number) {
+    this.selector1.fijar(valor);
+  }
+  
+  fijarSelector2(valor:number) {
+    this.selector2.fijar(valor);
+  }*/
+  /*articulos = [{codigo:1, descripcion:'papas', precio:10.55},
+               {codigo:2, descripcion:'manzanas', precio:12.10},
+               {codigo:3, descripcion:'melon', precio:52.30},
+               {codigo:4, descripcion:'cebollas', precio:17},
+               {codigo:5, descripcion:'calabaza', precio:20},
+              ];*/
+  
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*@ViewChild('selector1')selector1!: SelectornumericoComponent;
+  @ViewChild('selector2') selector2!: SelectornumericoComponent;
+  
+  fijarSelector1(valor:number) {
+    this.selector1.fijar(valor);
+  }
+  
+  fijarSelector2(valor:number) {
+    this.selector2.fijar(valor);
+  }*/
+  /*articulos = [{codigo:1, descripcion:'papas', precio:10.55},
+               {codigo:2, descripcion:'manzanas', precio:12.10},
+               {codigo:3, descripcion:'melon', precio:52.30},
+               {codigo:4, descripcion:'cebollas', precio:17},
+               {codigo:5, descripcion:'calabaza', precio:20},
+              ];*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*valor1: number;
+  valor2: number;
+  valor3: number;
+  resultado!: string;
+  constructor() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+  }
+  retornarAleatorio() {
+    return Math.trunc(Math.random() * 6) + 1;
+  }
+  tirar() {
+    this.valor1 = this.retornarAleatorio();
+    this.valor2 = this.retornarAleatorio();
+    this.valor3 = this.retornarAleatorio();
+    if (this.valor1==this.valor2 && this.valor1==this.valor3)
+      this.resultado='Ganó';
+    else
+      this.resultado='Perdió';
+  }*/
+  /*mensaje='';
+  actualizar(t: string) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }*/
+  /*@ViewChild('selector1')selector1!: SelectornumericoComponent;
+  @ViewChild('selector2') selector2!: SelectornumericoComponent;
+  
+  fijarSelector1(valor:number) {
+    this.selector1.fijar(valor);
+  }
+  
+  fijarSelector2(valor:number) {
+    this.selector2.fijar(valor);
+  }*/
+  /*articulos = [{codigo:1, descripcion:'papas', precio:10.55},
+               {codigo:2, descripcion:'manzanas', precio:12.10},
+               {codigo:3, descripcion:'melon', precio:52.30},
+               {codigo:4, descripcion:'cebollas', precio:17},
+               {codigo:5, descripcion:'calabaza', precio:20},
+              ];*/
+  articulos!: Object;
+
+  constructor(private http: HttpClient) { }     
+       
+  ngOnInit() {
+    this.http.get('http://scratchya.com.ar/vue/datos.php')
+    .subscribe(
+      result => {
+        this.articulos = result;
+      },
+      error => {
+        console.log('problemas');
+      }
+    );
+  }
+  
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DadoComponent } from './dado/dado.component';
+import { CronometroComponent } from './cronometro/cronometro.component';
+import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
+import { ListadoarticulosComponent } from './listadoarticulos/listadoarticulos.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DadoComponent,
+    CronometroComponent,
+    SelectornumericoComponent,
+    ListadoarticulosComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
